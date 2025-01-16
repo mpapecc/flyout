d3f34b4691784bde48eb64a62894cd554847c5ef
mpapechrc <111442881+mpapechrc@users.noreply.github.com>
2025-01-16 16:00:34 +0100

    sample app changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, ViewContainerRef } from '@angular/core';
-import { Flyout, NgFlyoutService } from 'ng-flyout';
+import { Flyout, HeaderButton, NgFlyoutService } from 'ng-flyout';
 import { FormComponent } from './form/form.component';
 
 @Component({
@@ -9,24 +9,39 @@ import { FormComponent } from './form/form.component';
 })
 export class AppComponent {
   title = 'flyout';
-  @ViewChild("flyouts", {read: ViewContainerRef}) flyouts!: ViewContainerRef;
+  @ViewChild("flyouts", { read: ViewContainerRef }) flyouts!: ViewContainerRef;
 
-  constructor(public flyoutService: NgFlyoutService){}
+  constructor(public flyoutService: NgFlyoutService) { }
 
   ngAfterViewInit(): void {
     this.flyoutService.setFlyoutContainerRef(this.flyouts);
+    this.openFlyout();
   }
 
-  openFlyout(){
+  openFlyout() {
     let flyout = new Flyout<FormComponent>();
-    flyout.content = FormComponent
+    flyout.content = FormComponent;
     flyout.title = "aloo";
+    flyout.onClose = () => console.log("on close event")
+
+    let doneButton = new HeaderButton();
+    doneButton.text = "Gotov sam";
+    doneButton.onClick = () => flyout.closeFlyout();
+    doneButton.cssClass = "red-button"
+
+    let consoleLogButton = new HeaderButton();
+    consoleLogButton.text = "Console Log";
+    consoleLogButton.onClick = () => {
+      console.log("Console button clicked");
+      flyout.closeFlyout();
+    }
+
+    flyout.headerButtons = [consoleLogButton, doneButton];
 
     this.flyoutService.create(flyout).subscribe(formComponent => {
-      formComponent.openAnotherFlyout = () =>{
+      formComponent.openAnotherFlyout = () => {
         let anotherflyout = new Flyout();
         anotherflyout.title = "Another Flyout";
-
         this.flyoutService.create(anotherflyout);
       }
     });
