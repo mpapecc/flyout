e574ee07e26e442c4b32c4d904252c869237220f
mpapechrc <111442881+mpapechrc@users.noreply.github.com>
2025-01-19 20:50:14 +0100

    remove flyouts container, instead append flyout html element to body node

--- projects/ng-flyout/src/lib/ng-flyout.service.ts
+++ projects/ng-flyout/src/lib/ng-flyout.service.ts
@@ -1,24 +1,33 @@
-import { Injectable, ViewContainerRef } from '@angular/core';
+import { ApplicationRef, createComponent, Inject, Injectable, Injector } from '@angular/core';
 import { NgFlyoutComponent } from './ng-flyout.component';
 import { Observable, skip } from 'rxjs';
 import { Flyout } from './models/flyout';
+import { DOCUMENT } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NgFlyoutService {
-  private flyoutsContainerRef!: ViewContainerRef;
 
-  setFlyoutContainerRef(flyoutsContainerRef: ViewContainerRef) {
-    this.flyoutsContainerRef = flyoutsContainerRef;
-  }
+  constructor(
+    private applicationRef: ApplicationRef,
+    private injector: Injector,
+    @Inject(DOCUMENT) private document: Document
+  ) { }
 
   create<T>(flyoutData: Flyout<T>): Observable<T> {
-    let flyout = this.flyoutsContainerRef.createComponent(NgFlyoutComponent);
-    flyout.instance.flyoutData = flyoutData;
-    flyout.instance.destroy = () => flyout.destroy();
-    
-    return flyout.instance.componentBehaviorSubject.pipe(skip(1)); 
+    let flyoutComponentRef = createComponent(NgFlyoutComponent, {
+      environmentInjector: this.applicationRef.injector,
+      elementInjector: this.injector
+    });
+
+    this.applicationRef.attachView(flyoutComponentRef.hostView);
+    this.document.body.appendChild(flyoutComponentRef.location.nativeElement);
+
+    flyoutComponentRef.instance.flyoutData = flyoutData;
+    flyoutComponentRef.instance.destroy = () => flyoutComponentRef.destroy();
+
+    return flyoutComponentRef.instance.componentBehaviorSubject.pipe(skip(1));
     // pipe(skip(1)) is "hack" since first value that componentBehaviorSubject emits is one that is assigend on initialize
     // and that is null. Next value is ComponentRef of component we render as flyout content
   }
